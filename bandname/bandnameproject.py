a1bbbaed935aa1adec7b27730ebc1eb94d64fc57
Rose Meenu <meenurose0@gmail.com>
2026-04-19 23:33:43 +0100

    first commit

--- bandname/bandnameproject.py
+++ bandname/bandnameproject.py
@@ -1,4 +1,4 @@
-print ("Welcome to the Band Name Generator.")
-city =input("Whats the name of the city you grew up in?\n")
-pet = input("Whats is the name of a pet\n")
-print("your band name could be:"+city+""+pet)
+name = "jack" 
+print (len(name))
+name ="Meenu"
+print(len(name))
